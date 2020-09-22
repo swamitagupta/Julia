d27bd78e95545b9894bc978396d5c47cd6661d25
Swamita <swamita2001@gmail.com>
2020-09-22 19:17:32 +0530

    Extract text from images

--- ImageToText/ViewController.swift
+++ ImageToText/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
+    
+    let image  = UIImage(named: "sample")
+    var textString = ""
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
+    var request = VNRecognizeTextRequest(completionHandler: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.activity.isHidden = true
+        self.activity.stopAnimating()
     }
-
-
+    @IBAction func cameraTapped(_ sender: Any) {
+        activity.isHidden = false
+        activity.startAnimating()
+        self.textView.text = ""
+        recognizeText(image: image!)
+    }
+    
+    private func recognizeText(image: UIImage) {
+        textString = ""
+        request = VNRecognizeTextRequest(completionHandler: { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation]
+            else {
+                fatalError("Invalid Observation")
+            }
+            for observation in observations{
+                guard let topCandidate = observation.topCandidates(1).first
+                else {print("No candidate")
+                    continue
+                }
+                self.textString += "\n\(topCandidate.string)"
+                DispatchQueue.main.async {
+                    self.textView.text = self.textString
+                    //self.textView.text = "akyzsfncklxduncubsgy"
+                    self.activity.stopAnimating()
+                    self.activity.isHidden = true
+                }
+            }
+        })
+        
+        request.customWords = ["custom"]
+        request.minimumTextHeight = 0.032
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["en_US"]
+        request.usesLanguageCorrection = true
+        
+        let requests = [request]
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let img = image.cgImage else {
+                fatalError("Cant scan")
+            }
+            let handle = VNImageRequestHandler(cgImage: img, options: [:])
+            try? handle.perform(requests)
+        }
+    }
 }
 
