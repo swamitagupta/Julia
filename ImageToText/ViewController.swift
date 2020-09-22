9e24b322df992d15042eb974af25ce4a011c6093
Swamita <swamita2001@gmail.com>
2020-09-22 20:14:08 +0530

    Add imagePicker Methods

--- ImageToText/ViewController.swift
+++ ImageToText/ViewController.swift
@@ -9,39 +9,55 @@ import UIKit
 import Vision
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
     
-    let image  = UIImage(named: "sample")
+    var image  = UIImage(named: "sample")
     var textString = ""
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var activity: UIActivityIndicatorView!
     
+    let imagePicker = UIImagePickerController()
     var request = VNRecognizeTextRequest(completionHandler: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.activity.isHidden = true
         self.activity.stopAnimating()
+        imagePicker.delegate = self
     }
+    
     @IBAction func cameraTapped(_ sender: Any) {
-        activity.isHidden = false
-        activity.startAnimating()
-        self.textView.text = ""
-        recognizeText(image: image!)
+        
+        let alert = UIAlertController(title: "Import Image", message: "From where do you want to import your image?", preferredStyle: .actionSheet)
+
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+            self.imagePicker.sourceType = .photoLibrary
+            self.imagePicker.allowsEditing = false
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Capture from Camera", style: .default, handler: { action in
+            self.imagePicker.sourceType = .camera
+            self.imagePicker.allowsEditing = false
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        alert.view.tintColor = UIColor(named: "AlertGolden")
+        self.present(alert, animated: true)
     }
     
     //MARK: - Text to Speech
     
     @IBAction func voiceTapped(_ sender: Any) {
         let utterance = AVSpeechUtterance(string: textView.text)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
         utterance.rate = 0.5
 
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(utterance)
     }
+    
     //MARK: - Extract text using Vision
     
     private func recognizeText(image: UIImage) {
@@ -65,7 +81,6 @@ class ViewController: UIViewController {
                 }
             }
         })
-        
         request.customWords = ["custom"]
         request.minimumTextHeight = 0.032
         request.recognitionLevel = .accurate
@@ -84,3 +99,21 @@ class ViewController: UIViewController {
     }
 }
 
+//MARK: - UI ImagePicker Methods
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else { fatalError("Failed to load image!") }
+        image = uiImage
+        imageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        activity.isHidden = false
+        activity.startAnimating()
+        self.textView.text = ""
+        recognizeText(image: image!)
+    }
+}
