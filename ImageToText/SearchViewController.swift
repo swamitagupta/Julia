befb46d76ae604d9323511895e5a1456ac000d44
Swamita <swamita2001@gmail.com>
2020-09-22 23:30:09 +0530

    Add Speech Recognition

---
+++ ImageToText/SearchViewController.swift
@@ -0,0 +1,138 @@
+//
+//  SearchViewController.swift
+//  ImageToText
+//
+//  Created by Swamita on 22/09/20.
+//
+
+import UIKit
+import Speech
+
+class SearchViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var micButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        micButton.isEnabled = false
+        speechRecognizer!.delegate = self
+            
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+                
+            var isButtonEnabled = false
+                
+            switch authStatus { 
+                case .authorized:
+                    isButtonEnabled = true
+                    
+                case .denied:
+                    isButtonEnabled = false
+                    print("User denied access to speech recognition")
+                    
+                case .restricted:
+                    isButtonEnabled = false
+                    print("Speech recognition restricted on this device")
+                    
+                case .notDetermined:
+                    isButtonEnabled = false
+                    print("Speech recognition not yet authorized")
+                }
+                
+            OperationQueue.main.addOperation() {
+                    self.micButton.isEnabled = isButtonEnabled
+                }
+        }
+        
+    }
+    
+    @IBAction func micTapped(_ sender: Any) {
+        if audioEngine.isRunning {
+                audioEngine.stop()
+                recognitionRequest?.endAudio()
+                micButton.isEnabled = false
+                micButton.setTitle("Start Recording", for: .normal)
+            } else {
+                startRecording()
+                micButton.setTitle("Stop Recording", for: .normal)
+            }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer!.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.micButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        textView.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            micButton.isEnabled = true
+        } else {
+            micButton.isEnabled = false
+        }
+    }
+}
