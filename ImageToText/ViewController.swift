cafc9b08dd812698f565f3eed0d23b61c7fd6dc8
Swamita <swamita2001@gmail.com>
2020-09-22 18:08:14 +0530

    Initial Commit

---
+++ ImageToText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageToText
+//
+//  Created by Swamita on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
